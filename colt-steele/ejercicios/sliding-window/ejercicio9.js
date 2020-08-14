a1ec267217b62ae01a54ca79cff51c0ba74bdc0d
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-08-13 23:34:20 -0500

    EL fucking ejercicio9 longestSubstring pirateado pero casi entendido

--- colt-steele/ejercicios/sliding-window/ejercicio9.js
+++ colt-steele/ejercicios/sliding-window/ejercicio9.js
@@ -15,31 +15,26 @@ console.log('********** Ejercicio 9 ***********');
 
 const c = console.log;
 
-function findLongestSubstring(str){
-    if(!str) return 0;
+function findLongestSubstring(str) {
     let longest = 0;
     let seen = {};
     let start = 0;
-    for(let i=0; i < str.length; i++){
-        let char = str[i];
-       
-         if(seen[char]) {
-            //c('seen', seen);
-            // c('i=',i);
-            // c('start',start);
-            start = seen[char];
-         }
-
-         longest = Math.max(longest, i - start + 1);
-
-         seen[char] = i + 1;
-
+   
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+      if (seen[char]) {
+         
+        start = Math.max(start, seen[char]);
+      }
+      // index - beginning of substring + 1 (to include current in count)
+      longest = Math.max(longest, i - start + 1);
+      // store the index of the next char so as to not double count
+      seen[char] = i + 1;
     }
-  
-
+    c('seen:',seen);
     return longest;
-}
+  }
 
 
 console.log('********** Solucion 1 ***********');
-console.log(findLongestSubstring('thisishowwedoit'));
+console.log(findLongestSubstring('longestsubstring'));
