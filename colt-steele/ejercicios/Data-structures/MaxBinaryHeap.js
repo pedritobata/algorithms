294226ccdb11ff6a6574901c03b1c2b5e64a63a5
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-10-15 08:30:49 -0500

    extractMax() implemented

--- colt-steele/ejercicios/Data-structures/MaxBinaryHeap.js
+++ colt-steele/ejercicios/Data-structures/MaxBinaryHeap.js
@@ -22,6 +22,50 @@ class MaxBinaryHeap{
         }
     }
 
+    extractMax(){
+        const max = this.values[0];
+        const end = this.values.pop();
+        if(this.values.length > 0){//si el array no esta vacio
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return max;
+    }
+
+    sinkDown(){
+        let idx = 0;
+        
+        const length = this.values.length;
+        while(true){
+            let element = this.values[idx];
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(element < leftChild){
+                    swap = leftChildIdx;
+                }
+            }
+
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx];
+                if((swap == null && element < rightChild) ||
+                   (swap != null && rightChild > leftChild)){
+                    swap = rightChildIdx;
+                }
+
+            }
+            if(swap == null) break;
+
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+
+        }
+    }
+
 }
 
 const maxBinaryHeap = new MaxBinaryHeap();
@@ -34,3 +78,7 @@ maxBinaryHeap.insert(45);
 console.log("Heap:", maxBinaryHeap);
 maxBinaryHeap.insert(199);
 console.log("Heap:", maxBinaryHeap);
+maxBinaryHeap.extractMax();
+console.log("Heap extract:", maxBinaryHeap);
+maxBinaryHeap.extractMax();
+console.log("Heap extract:", maxBinaryHeap);
