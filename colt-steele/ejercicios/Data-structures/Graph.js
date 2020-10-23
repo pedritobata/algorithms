6e95022e8ed5bbdd61ee4bab7a5078a93d94d5dd
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-10-23 07:53:06 -0500

    Traversing a graph dfs recursevely

--- colt-steele/ejercicios/Data-structures/Graph.js
+++ colt-steele/ejercicios/Data-structures/Graph.js
@@ -20,6 +20,31 @@ class Graph{
             v => v !== vertex1
         );
     }
+
+    removeVertex(vertex){
+        while(this.adjacencyList[vertex].length){
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+    }
+
+    depthFirstRecursive(start){
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList; //truco para acceder al this de afuera , osea a mi Graph
+        (function dfs(vertex){
+            //base case here??
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    dfs(neighbor);
+                }
+            });
+        })(start);
+        return result;
+    }
 }
 
 const graph = new Graph();
@@ -32,3 +57,22 @@ graph.addEdge("jhon", "tomas");
 console.log(graph);
 graph.removeEdge("tomas", "jhon");
 console.log(graph);
+graph.removeVertex("jhon");
+console.log(graph);
+
+const graphtoTraverse = new Graph();
+graphtoTraverse.addVertex("A");
+graphtoTraverse.addVertex("B");
+graphtoTraverse.addVertex("C");
+graphtoTraverse.addVertex("D");
+graphtoTraverse.addVertex("E");
+graphtoTraverse.addVertex("F");
+
+graphtoTraverse.addEdge("A","B");
+graphtoTraverse.addEdge("A","C");
+graphtoTraverse.addEdge("B","D");
+graphtoTraverse.addEdge("C","E");
+graphtoTraverse.addEdge("D","E");
+graphtoTraverse.addEdge("D","F");
+graphtoTraverse.addEdge("E","F");
+console.log("Traversed:",graphtoTraverse.depthFirstRecursive("A"));
