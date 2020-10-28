5f1988d25831a280949e834f7297cffc989d394f
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-10-28 07:34:56 -0500

    Dijkstra algorithm , weighted graph created

---
+++ colt-steele/ejercicios/Data-structures/Dijkstra.js
@@ -0,0 +1,23 @@
+class WeightedGraph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+
+    addEdge(vertex1, vertex2, weight){
+        this.adjacencyList[vertex1].push({node: vertex2, weight});
+        this.adjacencyList[vertex2].push({node: vertex1, weight});
+    }
+}
+
+const weightedGraph = new WeightedGraph();
+weightedGraph.addVertex("A");
+weightedGraph.addVertex("B");
+weightedGraph.addVertex("C");
+weightedGraph.addEdge("A", "B", 9);
+weightedGraph.addEdge("A", "C", 5);
+weightedGraph.addEdge("B", "C", 7);
+console.log("weighted graph:", weightedGraph.adjacencyList);
